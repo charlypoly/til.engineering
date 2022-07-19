36741035d83bcbdb3f626bc29b32f1efacc47893
Charly POLY <cpoly55@gmail.com>
2022-07-19 12:35:08 +0200

    chore: simpleanalytics setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+      <noscript>
+        {/* eslint-disable @next/next/no-img-element */}
+        <img
+          src="https://queue.simpleanalyticscdn.com/noscript.gif"
+          alt=""
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </noscript>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
